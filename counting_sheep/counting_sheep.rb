08239b91b3568d1da25a8b3d1f02a0123369ba0c
Toby Clarke <toby.clarke@simplybusiness.co.uk>
2017-05-04 14:30:29 +0100

    Add logic for processing input

--- counting_sheep/counting_sheep.rb
+++ counting_sheep/counting_sheep.rb
@@ -1,7 +1,32 @@
 class CountingSheep
+  attr_reader :seen_numbers
+
+  def initialize
+    @seen_numbers = []
+    @multiplier = 1
+  end
 
   def process(input)
-    'INSOMNIA'
+    if input == 0
+      'INSOMNIA'
+    else
+      while @seen_numbers.count < 10 do
+        @current_number = input * @multiplier
+        add_new_numbers_to_seen_numbers
+        @multiplier += 1
+      end
+      @current_number
+    end
+  end
+
+private
+
+  def add_new_numbers_to_seen_numbers
+    @current_number.to_s.split('').each do |string_digit|
+      if !@seen_numbers.include? string_digit.to_i
+        @seen_numbers << string_digit.to_i
+      end
+    end
   end
 
 end
--- counting_sheep/spec/counting_sheep_spec.rb
+++ counting_sheep/spec/counting_sheep_spec.rb
@@ -4,7 +4,28 @@ describe CountingSheep do
 
   describe '#process' do
     it 'returns INSOMNIA when processing 0' do
-      expect(subject.process 0).to eq "INSOMNIA"
+      expect(subject.process 0).to eq 'INSOMNIA'
+    end
+
+    it 'returns 10 when processing 1' do
+      expect(subject.process 1).to eq 10
+    end
+
+    it 'returns 90 when processing 2' do
+      expect(subject.process 2).to eq 90
+    end
+
+    it 'returns 110 when processing 11' do
+      expect(subject.process 11).to eq 110
+    end
+
+    it 'returns 5076 when processing 1692' do
+      expect(subject.process 1692).to eq 5076
+    end
+
+    it 'correctly tracks seen unique digits' do
+      subject.process 1
+      expect(subject.seen_numbers).to eq [1,2,3,4,5,6,7,8,9,0]
     end
   end
 
